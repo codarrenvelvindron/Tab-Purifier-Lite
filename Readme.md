c2de4f5418fca95beac125fe1605d5ef9a82f6d0
Codarren Velvindron <codarren@hackers.mu>
2017-04-20 00:43:09 +0400

    Source files added

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Tab Purifier Lite
+
+A simple and effective Google Chrome Extension, designed to close all pr0n tabs in one click!
+
+Please feel free to report any bugs you may find as an issue or via Git Reports.
+
+
---
+++ background.js
@@ -0,0 +1,29 @@
+// Copyright (c) 2017 Codarren Velvindron @ hackers.mu
+// Tab Purifier Version 1.73
+// The purpose of Tab Purifier Lite is to close all porn tabs using only 1 button!
+
+function detectTab(){
+    chrome.tabs.query({
+
+    },  function(array_of_Tabs) {
+            arrayLength = array_of_Tabs.length;
+            for (var i = 0; i < arrayLength; i++) {
+                var tab = array_of_Tabs[i].id;
+                var title = array_of_Tabs[i].title;
+
+//Create a blacklist containing words common to bad sites
+                var blacklist = ["Porn", "porn", "Porn,", "Tube", "Tubes", "Adult", "adult", "sex", "Sex", "sexy", "Sexy", "Amateur", "amateur", "Lingerie", "lingerie", "cam", "Cam", "tube", "tubes","Girl", "Girls", "webcam", "Webcam", "Webcams", "Flirt", "flirt", "Hot", "hot", "Chat", "chat", "Jasmin", "free", "Boobs", "boobs", "tits", "panty", "panties", "Sexy", "sexiest", "Sexiest", "beeg.","Nude", "nude", "nudes", "Nudes", "XXX", "Pics", "Fling", "18?", "Ass", "ass", "boobies", "Archive", "Femdom", "femdom", "BDSM", "SKINN","Naked", "naked", "Babes", "Babe", "babe", "NSFW", "nsfw", "Teen", "teen", "StreamPorno", "Milf", "milf", "LegalPorno", "HD", "Porno", "Sesso", "Playboy", "erotic", "erotica.", "Pornstar", "Sensations", "sensation","Sensation","Desire","Tolerance", "PornMastermind.com", "Models", "Model", "model", "MyFreeCams", "Skeet", "NWSGifs", "Thenewporn", "Updatetube", "Wank", "wank", "Female", "female", "fapdu", "ElephantTube", "Clips", "Private", "Anal", "Fetish", "free-porn-sites", "free-porn-site", "fake", "Fake", "Fappyness"];
+                var words = title.split(" ");
+                var detectBad = words.filter (function(a) {
+            return blacklist.indexOf(a) != -1;
+        });
+
+    var check = detectBad.length;
+    if (check>0){
+        chrome.tabs.remove(tab, function() { });
+    };           }
+    });
+};
+
+chrome.browserAction.onClicked.addListener(detectTab);
+
